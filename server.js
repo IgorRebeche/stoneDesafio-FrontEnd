3a59c9acef065d82396546a03c614423aa4c4668
IGOR REBECHE ROSA <igorrebeche@gmail.com>
2019-07-01 11:02:55 -0300

    src

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/stoneDesafio-FrontEnd'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/stoneDesafio-FrontEnd/index.html'));
+res.sendFile(path.join(__dirname+'/dist/stoneDesafio-FrontEnd/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
