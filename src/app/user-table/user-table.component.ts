d6f02e741620fe42d85ed54ae02895f1e353d2d7
Igor <igorrebeche@gmail.com>
2019-07-01 10:46:51 -0300

    added server

--- src/app/user-table/user-table.component.ts
+++ src/app/user-table/user-table.component.ts
@@ -47,7 +47,7 @@ export class UserTableComponent implements OnInit {
       //PEGAR ID data['position'], ACHAR USUARIO, DELETAR USUARIO
       console.log(data);
       //DELETAR ITEM DA LISTA
-      this.http.post('http://localhost:3000/api/users/remove', data.userData).subscribe(
+      this.http.post('https://igorr-app.herokuapp.com/api/users/remove', data.userData).subscribe(
         (data) => {
           console.log('atualizar lista');
           this.updateList();
@@ -57,7 +57,7 @@ export class UserTableComponent implements OnInit {
 
     } else if (data['acao'] == 'Salvar') {
       // ATUALIZAR NO BD
-      this.http.post('http://localhost:3000/api/users/update', data.userData).subscribe(
+      this.http.post('https://igorr-app.herokuapp.com/api/users/update', data.userData).subscribe(
         (response: any) => {
           if(response.status == 'error'){
             alert('Algum erro ocorreu');
@@ -73,20 +73,20 @@ export class UserTableComponent implements OnInit {
     } else if (data['acao'] == 'Criar') {
       //Enviar usuario para BD
 
-      this.http.post('http://localhost:3000/api/users/add', data.userData).subscribe(
+      this.http.post('https://igorr-app.herokuapp.com/api/users/add', data.userData).subscribe(
         (response: any) => {
           console.log('atualizar lista');
           this.updateList();
         },
         (error) => {
-
+          console.log('error', error);
         }
       )
       //Caso nao registre-o
     }
   }
   updateList(){
-    this.http.get('http://localhost:3000/api/users').subscribe((response: any) => {
+    this.http.get('https://igorr-app.herokuapp.com/api/users').subscribe((response: any) => {
       this.dataSource = new MatTableDataSource(response);
 
       this.dataSource.paginator = this.paginator;
